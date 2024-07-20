dcd91a19d25ef5f93a0d9c3826b499ce7c0a33b1
SURAJGODEXE <imepay288@gmail.com>
2024-07-20 16:07:59 +0545

    Add files via upload

---
+++ fps.py
@@ -0,0 +1,64 @@
+import os
+import time
+
+
+print()
+print()
+print()
+print ()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print("""\033[92m       
+███████╗███████╗
+██╔════╝██╔════╝
+█████╗░░█████╗░░
+██╔══╝░░██╔══╝░░
+██║░░░░░██║░░░░░
+╚═╝░░░░░╚═╝░░░░░
+
+
+██████╗░░█████╗░███╗░░██╗███████╗██╗░░░░░
+██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░
+██████╔╝███████║██╔██╗██║█████╗░░██║░░░░░
+██╔═══╝░██╔══██║██║╚████║██╔══╝░░██║░░░░░
+██║░░░░░██║░░██║██║░╚███║███████╗███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚══════╝""")
+print("\033[95m------------------------------------------------")
+print("\033[93m𝔸𝕦𝕥𝕙𝕠𝕣:\033[96mSINZOO LUVVS YOUU \033[92m[SINZO GOD CHEAT✓]")
+print()
+print()
+input("\033[92m[\033[0m~\033[92m]\033[0m 1.AIMBOT   2.AIMLOCK  3.NO RECOIL   4.ALL IN ONE \033[95m:")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Please wait..")
+time.sleep(2)
+print("\033[92m[\033[0m=\033[92m]\033[96m [\033[93m1. FREE FIRE/\033[96m2. FREE FIRE MAX /]")
+input("\033[92m[\033[0m~\033[92m]\033[0m SELECT APK \033[96m:")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Please wait..")
+time.sleep(2)
+print("\033[92m[\033[0m=\033[92m]\033[96m [\033[93m1.MAIN ID USE /\033[96m2.GUEST ACCOUNT USE/]")
+input("\033[92m[\033[0m~\033[92m]\033[0m SELECT TYPE\033[95m:")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Please wait..")
+time.sleep(2)
+input("\033[92m[\033[0m~\033[92m]\033[0m Start the Config? (yes/no) \033[96m:")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Starting config...")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Customizing File!...")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[92m Config Running in device...")
+time.sleep(2)
+print("\033[92m[\033[0m~\033[92m]\033[0mStatus\033[96m:SUCCESS")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[91m DON'T REMOVE TERMUX FROM BACKGROUND..")
+time.sleep(2)
+print("\033[92m[\033[0m*\033[92m]\033[91m MOBILE PANEL IS NOT AVAILABLE RIGHT NOW BECAUSE OF FREE FIRE PATCH UPDATE TRY AGAIN LATER OR CONTACT WITH DEVELOPER..")
+time.sleep(2)
+print("\033[96m<===SINZO GOD CHEAT===>")
